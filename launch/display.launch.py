29dee99de151e93dfb2508e498bd83a1f68f4064
Austin Greisman <austin.mfg@gmail.com>
2022-10-09 18:59:01 -0400

    Updates

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -5,11 +5,10 @@ import launch_ros
 import os
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='sim_bot_description').find('sim_bot_description')
-    default_model_path = os.path.join(pkg_share, 'src/description/sim_bot_description.urdf')
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='rover_rick_sim').find('rover_rick_sim')
+    default_model_path = os.path.join(pkg_share, 'src/description/rover_rick.urdf.xacro')
     parameters_file_path = os.path.join(pkg_share, 'config/dual_ekf.yaml')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
-    # default_nav2_config_path = os.path.join(pkg_share, "launch/nav2_params.yaml")
     world_path=os.path.join(pkg_share, 'world/my_world.sdf'),
     agriculture_path=os.path.join(pkg_share, 'world/actually_empty_world.world'),
     
@@ -24,15 +23,7 @@ def generate_launch_description():
         executable='joint_state_publisher',
         name='joint_state_publisher',
         parameters=[{"use_sim_time": True}]
-        # condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-
-    # joint_state_publisher_gui_node = launch_ros.actions.Node(
-    #     package='joint_state_publisher_gui',
-    #     executable='joint_state_publisher_gui',
-    #     name='joint_state_publisher_gui',
-    #     condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    # )
 
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
@@ -45,12 +36,12 @@ def generate_launch_description():
     spawn_entity = launch_ros.actions.Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-entity', 'sim_bot', '-topic', 'robot_description', '-z', '0.166', '-x', '-0.5', '-y', '32.0', '-R', '0.0', '-P', '0.0', '-Y', '0.0'],
+        arguments=['-entity', 'rover_rick', '-topic', 'robot_description', '-z', '0.166', '-x', '-0.5', '-y', '32.0', '-R', '0.0', '-P', '0.0', '-Y', '0.0'],
         parameters=[{"use_sim_time": True}],
         output='screen'
     )
     gps_imu_covariance_node = launch_ros.actions.Node(
-        package='sim_bot_description',
+        package='rover_rick_sim',
         executable='gps_imu_covariance',
         name='gps_imu_covariance',
         output='screen',
@@ -100,9 +91,9 @@ def generate_launch_description():
 	        output='screen',
             parameters=[parameters_file_path],
             # from -> to
-            remappings=[('/imu/data', '/demo/imu'),
-                        ('/gps/fix', '/demo/gps'), 
-                        ('/odometry/filtered', '/demo/odom_to_map')]           
+            remappings=[('/imu/data', '/sim/imu'),
+                        ('/gps/fix', '/sim/gps'), 
+                        ('/odometry/filtered', '/sim/odom_to_map')]           
 
            )
 
@@ -112,14 +103,6 @@ def generate_launch_description():
         name="nav2_costmap2d_node",
         parameters=[{"user_sim_time": True}],
         output="screen"
-    )
-    tf_ptz_node = launch_ros.actions.Node(
-        package='ptz_pkg2',
-        executable='ptz_tf_broadcaster',
-        name='ptz_tf_broadcaster',
-        parameters=[
-            {'PTZ_NAME': 'ptz'},
-        ]
     )
 
     joycon_node = launch_ros.actions.Node(
@@ -133,39 +116,22 @@ def generate_launch_description():
         executable='cmd_vel_conversion',
         name='cmd_vel_conversion',
         parameters=[
-            {'cmd_vel_topic' : '/demo/cmd_vel'},
+            {'cmd_vel_topic' : '/sim/cmd_vel'},
         ]
     )
-
-    # gazebo_image_compress_node = launch_ros.actions.Node(
-    #     package='sim_bot_description',
-    #     executable='gazebo_image_compress',
-    #     name='gazebo_image_compress',
-    # )
-
-
     
     return launch.LaunchDescription([
-        # launch.actions.DeclareLaunchArgument(name='gui', default_value='True', description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
-        # launch.actions.ExecuteProcess(cmd=['$(find xacro)/xacro', '--inorder', "'$(find sim_bot_description)/src/worlds_urdfs/agriculture_geometry.urdf.xacro'"], output='screen'),
         launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', agriculture_path], output='screen'),
-        # launch.actions.DeclareLaunchArgyment(name="params_file", default_value=)
 
         joint_state_publisher_node,
-        # joint_state_publisher_gui_node,
         robot_state_publisher_node,
         spawn_entity,
-        gps_imu_covariance_node,
-        # efk_odom,
-        # efk_map,
+        # gps_imu_covariance_node,
         static_publisher_node,
-        efk_nav,
-        # nav2_costmap2d_node,
+        # efk_nav,
         rviz_node,
-        tf_ptz_node,
-        joycon_node,
-        joy_converter_node
-        # gazebo_image_compress_node
+        # joycon_node,
+        # joy_converter_node
     ])
